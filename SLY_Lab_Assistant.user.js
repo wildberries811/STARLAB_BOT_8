881dd8f91dc05fb04d96939f1363ccb0e4727713
wildberries811 <164812821+wildberries811@users.noreply.github.com>
2026-01-01 12:56:31 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/STARLAB_BOT_8/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/STARLAB_BOT_8/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/STARLAB_BOT_8/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -3642,5 +3642,6 @@
     }
     await miningSelfDestruct(userFleets[2], -30, 30);
     */
+
 
 })();
